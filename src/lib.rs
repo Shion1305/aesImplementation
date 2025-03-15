f05d9ab51452b8cac925d771d5f2f5533ce93216
Shion Ichikawa <shion1305@gmail.com>
2025-03-15 23:51:36 +0900

    ✨ impl key expansion and test through log

--- src/lib.rs
+++ src/lib.rs
@@ -95,16 +95,34 @@ impl AES {
         aes_log::push(AES::state2block(*state));
     }
 
+    #[inline]
+    fn log_keys(keys: &[Word]) {
+        #[cfg(test)]
+        {
+            assert_eq!(keys.len() % 4, 0);
+            let mut i = 4;
+            while i <= keys.len() {
+                aes_log::push(AES::state2block([
+                    keys[i - 4],
+                    keys[i - 3],
+                    keys[i - 2],
+                    keys[i - 1],
+                ]));
+                i += 4;
+            }
+        }
+    }
+
     fn encrypt_block(&self, block: Block) -> Block {
         let state = AES::block2state(block);
         AES::log_state(&state);
-
-        // impl here
+        let round_key = self.key_expansion();
+        AES::log_keys(&round_key);
 
         AES::state2block(state)
     }
 
-    fn key_expansion(&self) {
+    fn key_expansion(&self) -> Vec<Word> {
         fn rot_word(word: Word) -> Word {
             [word[1], word[2], word[3], word[0]]
         }
@@ -169,6 +187,7 @@ impl AES {
             }
             wlist.push(word_xor(wlist[i - nk], temp));
         }
+        wlist
     }
 }
 
@@ -182,19 +201,69 @@ mod tests {
 
     #[test]
     fn test() {
+        const CIPHER_KEY: &[Block] = &[Block([
+            0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf,
+            0x4f, 0x3c,
+        ])];
+
+        const EXPANDED_KEYS: &[Block] = &[
+            Block([
+                0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf,
+                0x4f, 0x3c,
+            ]),
+            Block([
+                0xa0, 0xfa, 0xfe, 0x17, 0x88, 0x54, 0x2c, 0xb1, 0x23, 0xa3, 0x39, 0x39, 0x2a, 0x6c,
+                0x76, 0x05,
+            ]),
+            Block([
+                0xf2, 0xc2, 0x95, 0xf2, 0x7a, 0x96, 0xb9, 0x43, 0x59, 0x35, 0x80, 0x7a, 0x73, 0x59,
+                0xf6, 0x7f,
+            ]),
+            Block([
+                0x3d, 0x80, 0x47, 0x7d, 0x47, 0x16, 0xfe, 0x3e, 0x1e, 0x23, 0x7e, 0x44, 0x6d, 0x7a,
+                0x88, 0x3b,
+            ]),
+            Block([
+                0xef, 0x44, 0xa5, 0x41, 0xa8, 0x52, 0x5b, 0x7f, 0xb6, 0x71, 0x25, 0x3b, 0xdb, 0x0b,
+                0xad, 0x00,
+            ]),
+            Block([
+                0xd4, 0xd1, 0xc6, 0xf8, 0x7c, 0x83, 0x9d, 0x87, 0xca, 0xf2, 0xb8, 0xbc, 0x11, 0xf9,
+                0x15, 0xbc,
+            ]),
+            Block([
+                0x6d, 0x88, 0xa3, 0x7a, 0x11, 0x0b, 0x3e, 0xfd, 0xdb, 0xf9, 0x86, 0x41, 0xca, 0x00,
+                0x93, 0xfd,
+            ]),
+            Block([
+                0x4e, 0x54, 0xf7, 0x0e, 0x5f, 0x5f, 0xc9, 0xf3, 0x84, 0xa6, 0x4f, 0xb2, 0x4e, 0xa6,
+                0xdc, 0x4f,
+            ]),
+            Block([
+                0xea, 0xd2, 0x73, 0x21, 0xb5, 0x8d, 0xba, 0xd2, 0x31, 0x2b, 0xf5, 0x60, 0x7f, 0x8d,
+                0x29, 0x2f,
+            ]),
+            Block([
+                0xac, 0x77, 0x66, 0xf3, 0x19, 0xfa, 0xdc, 0x21, 0x28, 0xd1, 0x29, 0x41, 0x57, 0x5c,
+                0x00, 0x6e,
+            ]),
+            Block([
+                0xd0, 0x14, 0xf9, 0xa8, 0xc9, 0xee, 0x25, 0x89, 0xe1, 0x3f, 0x0c, 0xc8, 0xb6, 0x63,
+                0x0c, 0xa6,
+            ]),
+        ];
         setup();
         let plain_text = b"k\xc1\xbe\xe2.@\x9f\x96\xe9=~\x11s\x93\x17*";
-        let aes = AES::new(b"+~\x15\x16(\xae\xd2\xa6\xab\xf7\x15\x88\t\xcfO<")
-            .ok()
-            .unwrap();
+        let aes = AES::new(&CIPHER_KEY[0].0).ok().unwrap();
         let _encrypted = aes.encrypt_block(Block(*plain_text));
         let log = aes_log::get();
-        let expected = [plain_text];
+        let expected = [&[Block(*plain_text)], EXPANDED_KEYS].concat();
+        assert_eq!(log.borrow().len(), expected.len());
         log.borrow()
             .iter()
             .zip(expected.into_iter())
             .for_each(|(a, b)| {
-                assert_eq!(&a.0, b);
+                assert_eq!(a, &b);
             });
     }
 
