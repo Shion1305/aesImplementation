3a44b134717c87986bdce28b0de74d79738f3768
Shion Ichikawa <shion1305@gmail.com>
2025-03-15 21:25:42 +0900

    ✨ define Block

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,12 @@
 mod log;
 
+const AES_BLOCK_SIZE: usize = 16;
+
 #[cfg(test)]
 prog_log!(aes_log, u32);
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Block([u8; AES_BLOCK_SIZE]);
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
